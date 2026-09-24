693e8d291e6d677d82d0b0e571f319d775551e0f
shogerafghan333-dotcom <shogerafghan333@gmail.com>
2026-09-25 01:34:16 +0800

    Add keylist with VIP and DEMO entries

--- keylist.lua
+++ keylist.lua
@@ -1,1 +1,16 @@
-
+return {
+    ["VIP_001"] = {
+        type = "VIP",
+        expiry = "2027-12-31",
+        valid = true,
+        max_devices = 1,
+        SLOT = "1"
+    },
+    ["DEMO123"] = {
+        type = "DEMO",
+        expiry = "2026-08-01",
+        valid = true,
+        max_devices = 5,
+        SLOT = "demo"
+    }
+}
